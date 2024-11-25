c093fb43634c8b37a7f78bd6e6313a21a658256a
Rob Gale <robagale@aol.com>
2024-11-25 16:31:52 -0500

    Added demo elevator and drive ports

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -6,12 +6,13 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 
 public class Elevator extends SubsystemBase {
   /** Creates a new Elevator. */
   //TODO: Get the right port for the elevator and switch
-  public PWMSparkMax m_motor = new PWMSparkMax(0);
+  public PWMSparkMax m_motor = new PWMSparkMax(Constants.ElevatorConstants.kElevatorMotorPort);
   
   public Elevator() {}
 

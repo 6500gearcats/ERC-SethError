96c896307cd180c4ced4bc2c896bcf24405e5f05
Pranav Vanparty <pvanparty@gmail.com>
2024-11-27 18:04:41 -0500

    Add Turbo Mode indicator to SmartDashboard

--- src/main/java/frc/robot/commands/GoTurbo.java
+++ src/main/java/frc/robot/commands/GoTurbo.java
@@ -4,11 +4,13 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.RomiDrivetrain;
 
 public class GoTurbo extends Command {
   private RomiDrivetrain m_drivetrain;
+  private boolean turboActive = false;
 
   /** Creates a new GoTurbo. */
   public GoTurbo(RomiDrivetrain theDrivetrain) {
@@ -20,7 +22,10 @@ public class GoTurbo extends Command {
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    turboActive = true;
+    SmartDashboard.putBoolean("Turbo", turboActive);
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
@@ -32,6 +37,8 @@ public class GoTurbo extends Command {
   @Override
   public void end(boolean interrupted) {
     m_drivetrain.arcadeDrive(0, 0);
+    turboActive = false;
+    SmartDashboard.putBoolean("Turbo", turboActive);
   }
 
   // Returns true when the command should end.
